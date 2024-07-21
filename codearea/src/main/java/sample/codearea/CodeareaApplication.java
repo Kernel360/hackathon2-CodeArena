31a16c94a9912e180503f0f5dfe972f7d29af25b
kimminkyeu <minkyeki@student.42seoul.kr>
2024-07-21 15:08:53 +0900

    initial commit

---
+++ codearea/src/main/java/sample/codearea/CodeareaApplication.java
@@ -0,0 +1,13 @@
+package sample.codearea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeareaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeareaApplication.class, args);
+	}
+
+}
---
+++ codearea/src/test/java/sample/codearea/CodeareaApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.codearea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeareaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
