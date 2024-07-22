f73daaf0d086cb858d4d3d9f53d74263a054c93e
kimminkyeu <minkyeki@student.42seoul.kr>
2024-07-22 21:29:18 +0900

    feat: 초안 기능 구현

--- codearea/src/main/java/sample/codearea/service/UserService.java
+++ codearea/src/main/java/sample/codearea/service/UserService.java
@@ -3,8 +3,6 @@ package sample.codearea.service;
 import org.springframework.stereotype.Service;
 
 /**
- * - NOTE: nickname 은 반드시 유일해야 한다.
- * 	 DB에 닉네임이 중복일 경우, 임의의 닉네임을 생성해야 합니다.
  * - NOTE: scrap은 질문당 1개만 가능.
  */
 @Service
