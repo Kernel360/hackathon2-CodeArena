69d48148211461cc15acbe28629e14dfd295466c
sangwonsheep <swonyy1709@naver.com>
2024-07-22 17:35:03 +0900

    Answer 관련 Dto 생성, Service, Converter 수정, 테스트를 위해 TestDataInit 생성, 순환 참조 문제 때문에 AnswerEntity, UserEntity 수정

--- codearea/src/main/java/sample/codearea/service/AnswerConverter.java
+++ codearea/src/main/java/sample/codearea/service/AnswerConverter.java
@@ -1,13 +1,14 @@
 package sample.codearea.service;
 
 import sample.codearea.dto.AnswerRequestDto;
+import sample.codearea.dto.AnswerResponseDto;
 import sample.codearea.entity.AnswerEntity;
 
 public class AnswerConverter {
 
-    public AnswerRequestDto toDto(AnswerEntity entity) {
-        return AnswerRequestDto.builder()
-                .userName(entity.getUser().getNickname())
+    public AnswerResponseDto toDto(AnswerEntity entity) {
+        return AnswerResponseDto.builder()
+                .modifiedAt(entity.getUpdatedAt())
                 .content(entity.getContent())
                 .build();
     }
--- codearea/src/main/java/sample/codearea/service/AnswerService.java
+++ codearea/src/main/java/sample/codearea/service/AnswerService.java
@@ -3,6 +3,7 @@ package sample.codearea.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import sample.codearea.dto.AnswerRequestDto;
+import sample.codearea.dto.AnswerTestDto;
 import sample.codearea.entity.AnswerEntity;
 import sample.codearea.entity.UserEntity;
 import sample.codearea.repository.AnswerRepository;
@@ -11,6 +12,9 @@ import sample.codearea.repository.UserRepository;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * 한 사람당 하나의 답변만 작성할 수 있다.
+ */
 @Service
 @RequiredArgsConstructor
 public class AnswerService {
@@ -26,16 +30,23 @@ public class AnswerService {
         return answerRepository.findAll();
     }
 
-    public AnswerEntity save(AnswerRequestDto answerRequestDto) {
-//        UserEntity user = userRepository.findById().get();
-//
-//        user.getNickname()
-//
-        return null;
+    public AnswerEntity save(AnswerTestDto answerRequestDto) {
+        UserEntity user = userRepository.findById(answerRequestDto.getUserId()).get();
+
+        AnswerEntity answer = AnswerEntity.builder()
+                .user(user)
+                .content(answerRequestDto.getContent())
+                .build();
+
+        AnswerEntity save = answerRepository.save(answer);
+        return save;
     }
 
-    public void update(AnswerRequestDto answerEntity) {
+    public void update(Long answerId, AnswerTestDto answerRequestDto) {
+        AnswerEntity answer = answerRepository.findById(answerId).orElseThrow();
 
+        answer.setContent(answerRequestDto.getContent());
+        answerRepository.save(answer);
     }
 
     public void delete(Long answerId) {
