dfddd9261b07a3eb3cfa51a16531d3f52207a794
sangwonsheep <swonyy1709@naver.com>
2024-07-22 22:38:59 +0900

    Answer 관련 수정, Test Data 수정

--- codearea/src/main/java/sample/codearea/service/AnswerService.java
+++ codearea/src/main/java/sample/codearea/service/AnswerService.java
@@ -3,6 +3,7 @@ package sample.codearea.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import sample.codearea.dto.AnswerRequestDto;
+import sample.codearea.dto.AnswerResponseDto;
 import sample.codearea.dto.AnswerTestDto;
 import sample.codearea.entity.AnswerEntity;
 import sample.codearea.entity.UserEntity;
@@ -11,6 +12,7 @@ import sample.codearea.repository.UserRepository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * 한 사람당 하나의 답변만 작성할 수 있다.
@@ -21,16 +23,19 @@ public class AnswerService {
 
     private final UserRepository userRepository;
     private final AnswerRepository answerRepository;
+    private final AnswerConverter answerConverter;
 
     public Optional<AnswerEntity> findById(Long answerId) {
         return answerRepository.findById(answerId);
     }
 
-    public List<AnswerEntity> findAll() {
-        return answerRepository.findAll();
+    public List<AnswerResponseDto> findAll() {
+        return answerRepository.findAll().stream()
+                .map(answerConverter::toDto)
+                .collect(Collectors.toList());
     }
 
-    public AnswerEntity save(AnswerTestDto answerRequestDto) {
+    public AnswerResponseDto save(AnswerTestDto answerRequestDto) {
         UserEntity user = userRepository.findById(answerRequestDto.getUserId()).get();
 
         AnswerEntity answer = AnswerEntity.builder()
@@ -38,8 +43,10 @@ public class AnswerService {
                 .content(answerRequestDto.getContent())
                 .build();
 
+
         AnswerEntity save = answerRepository.save(answer);
-        return save;
+        AnswerResponseDto answerResponseDto = answerConverter.toDto(save);
+        return answerResponseDto;
     }
 
     public void update(Long answerId, AnswerTestDto answerRequestDto) {
