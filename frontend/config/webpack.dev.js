da61f3cef32d3043c3c05ac997d20c5d9d9758e8
Lee kangmin <km2535@naver.com>
2024-07-25 13:48:58 +0900

    feature : port 수정

--- frontend/config/webpack.dev.js
+++ frontend/config/webpack.dev.js
@@ -8,7 +8,7 @@ module.exports = merge(common, {
     open: false,
     hot: true,
     compress: true,
-    port: 8081,
+    port: 4445,
     historyApiFallback: true,
     liveReload: true,
   },
--- frontend/src/components/question-detail/answers/AnswerItem.tsx
+++ frontend/src/components/question-detail/answers/AnswerItem.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 import { useAuth } from "@/context/AuthContext";
 import { AnswerType } from "@/types/AnswerType";
 import { Comment } from "./comment/Comment";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {  useParams } from "react-router-dom";
 import { updateRequestAnswer } from "@/api/requestAnswer";
 
@@ -18,12 +18,14 @@ export const AnswerItem = ({answerItem, deleteRequestAnswer}:{answerItem:AnswerT
         const questionId = param?.questionId ||"" ;
         updateRequestAnswer(newContent,questionId, answerItem.answerId).then(()=>window.location.reload())
     }
-    
+    useEffect(()=>{
+        setAnswerContent(answerItem.content)
+    },[answerItem.content])
     return (
         <>
             <div className="p-4 mb-4 bg-white shadow-md rounded-md">
                 <div className="text-gray-500 text-sm">ID: {answerItem.answerId}</div>
-                <input className="mt-2 text-lg font-semibold" onChange={(e:any)=>setAnswerContent(e.target.value)} defaultValue={answerItem.content} disabled = {user?.nickname !== answerItem.nickname}/>
+                <input className="mt-2 text-lg font-semibold" onChange={(e:any)=>setAnswerContent(e.target.value)} value={answerContent} disabled = {user?.nickname !== answerItem.nickname}/>
                 <div className="mt-4 text-gray-400 text-xs">수정된 날짜: {new Date(answerItem.modifiedAt).toLocaleString()}</div>
                 <div className="mt-2 text-right text-blue-500 font-medium">{answerItem.nickname}</div>
                 {user?.nickname == answerItem.nickname ? 
